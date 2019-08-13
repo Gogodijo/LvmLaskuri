ad31e3218a3d5a37dc244a4534817ea2b71f9400
Joni Vilokki <36036801+Gogodijo@users.noreply.github.com>
2019-08-13 16:29:02 +0300

    Jee
    Jee

--- app/src/main/java/com/example/lvmlaskuri/MainActivity.kt
+++ app/src/main/java/com/example/lvmlaskuri/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.lvmlaskuri
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        helpButton.setOnClickListener {
+            helpClick()
+        }
+
+    }
+    fun helpClick(){
+
+    }
+    fun calculateLVM(){
+
     }
 }
